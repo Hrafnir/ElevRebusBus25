91f1197c7377e4c4253dd6d9908f936fdf6e290a
Hrafnir <eirik.gaarde@gmail.com>
2025-05-23 14:10:36 +0200

    Update post3.js

--- posts/post3.js
+++ posts/post3.js
@@ -1,15 +1,17 @@
-/* Version: #40 */
+/* Version: #56 */
 // Filnavn: posts/post3.js
 
 function definePost3() {
     return {
         id: 3,
-        name: "Lavvoen Øverby",
+        name: "Lavvoen Øverby (Bildeoppgave)", // Oppdatert navn for klarhet
         lat: 60.80121161360927,
         lng: 10.645440903323017,
         type: "standard",
-        question: "Hva er spørsmålet ved Lavvoen på Øverby?", // ERSTATT
-        correctAnswer: "SVARPOST3", // ERSTATT
+        question: "Hva er svaret på oppgaven vist i bildet?", // Denne teksten vises over bildet i HTML
+        correctAnswer: "11", // Riktig svar er 11
+        // maxAttempts og pointsPerCorrect vil bruke standard logikk fra core.js (5 forsøk, poengsum 5-0)
+        // initUI er ikke nødvendig for denne standardoppgaven
     };
 }
-/* Version: #40 */
+/* Version: #56 */
