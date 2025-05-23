0a3cc7bc962e139d57860e842cf6c6382fd8f320
Hrafnir <eirik.gaarde@gmail.com>
2025-05-23 17:37:10 +0200

    Update post6.js

--- posts/post6.js
+++ posts/post6.js
@@ -1,16 +1,16 @@
-/* Version: #60 */
+/* Version: #61 */
 // Filnavn: posts/post6.js
 
 function definePost6() {
     return {
         id: 6,
-        name: "Matematikknøtt", // Du må oppgi faktisk poststed og koordinater
-        lat: 0, // ERSTATT MED KORREKT LATITUDE
-        lng: 0, // ERSTATT MED KORREKT LONGITUDE
+        name: "Hunn Gravlund (Matematikknøtt)", // Oppdatert navn
+        lat: 60.80202682020165, // Korrekt latitude
+        lng: 10.673687047853834, // Korrekt longitude
         type: "standard",
         question: "Leo og Paul er brødre.\n- Jeg er tre år yngre enn Paul, men om seks år er Paul dobbelt så gammel som jeg er nå, sier Leo.\n\nHvor gamle er Leo og Paul til sammen?",
         correctAnswer: "21",
         // maxAttempts og pointsPerCorrect vil bruke standard logikk fra core.js
     };
 }
-/* Version: #60 */
+/* Version: #61 */
